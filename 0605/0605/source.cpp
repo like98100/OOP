7ea67e2eb762f4699da6a9c36105d815d366cbbc
like <like98100@naver.com>
2018-06-05 12:52:27 +0900

    0605

---
+++ 0605/0605/source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+//생성자 매개 변수 전달
+//디폴트 값
+
+//상속 지정	↓
+//private	:: 기본 클래스 public/protected 를 private로 계승
+//public	::  기본 클래스 public/protected 를 그대로 계승
+//protected	:: 기본 클래스 public/protected 를 protected로 계승
+
+//다중 상속
+int main()
+{
+
+	return 0;
+}
